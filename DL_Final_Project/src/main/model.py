25cd187b59c7ab3c0998923e74b3c4a2ce627edc
nenamalikah <nmb72@georgetown.edu>
2024-11-27 16:57:20 -0500

    Update model.py

--- DL_Final_Project/src/main/model.py
+++ DL_Final_Project/src/main/model.py
@@ -1,11 +1,11 @@
 #%%
 import pandas as pd
-from sklearn.model_selection import train_test_split
 import numpy as np
 from keras.applications.vgg16 import VGG16
-from keras.models import Sequential
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
 
-from keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Dense, Dropout, Flatten
 
 from keras.layers import Conv2D, MaxPooling2D
 import sys
@@ -14,6 +14,7 @@ sys.path.append("../")
 from components.utils import *
 #%%
 
+
 y1 = pd.read_excel('../../data/Celeb-real-sample.xlsx')
 y1 = y1['label']
 y0 = pd.read_excel('../../data/Celeb-synthesis-sample.xlsx')
@@ -21,43 +22,32 @@ y0 = y0['label']
 
 y = list(y1) + list(y0)
 
-real = image_to_array(mapping='../../data/Celeb-real-sample.xlsx',
+#%%
+real, y_real = image_to_array(mapping='../../data/Celeb-real-sample.xlsx',
                image_fp='../../data/Celeb-real-sample/Frames',
-               image_size=224)
-print(f'Shape of real: {real.shape}')
-fake = image_to_array(mapping='../../data/Celeb-synthesis-sample.xlsx',
+               image_size=224,
+                      split='train')
+
+fake, y_fake = image_to_array(mapping='../../data/Celeb-synthesis-sample.xlsx',
                image_fp='../../data/Celeb-synthesis-sample/Frames',
-               image_size=224)
-print(f'Shape of fake: {fake.shape}')
+               image_size=224,
+                      split='train')
+
 # Combine the arrays
 frames = np.concatenate((real, fake), axis=0)
-# creating the training and validation set
-print(f'Shape of frames: {frames.shape}')
-
-X_train, X_test, y_train, y_test = train_test_split(frames, y, random_state=6303, test_size=0.2, stratify = y)
-# creating the base model of pre-trained VGG16 model
-
-base_model = VGG16(weights='imagenet', include_top=False)
-
-# extracting features for training frames
-
-X_train = base_model.predict(X_train)
-print(f'X Train shape: {X_train.shape}')
-
-# extracting features for validation frames
-
-X_test = base_model.predict(X_test)
-print(f'X Test shape: {X_test.shape}')
+y = y_real + y_fake
 
 #%%
-X_train = X_train.reshape(80, 7*7*512)
-X_test = X_test.reshape(20, 7*7*512)
+# Create Train-Test Splits and Extract Features from Pretrained Model
 
+X_train, X_test, y_train, y_test = process_input(X=frames, y=y, model='ResNet50', num_classes=2, input_type='train')
+input_shape = int(X_train.shape[1])
+#
 #%%
+# Define Classification Layers
+
 model = Sequential()
-
-model.add(Dense(1024, activation='relu', input_shape=(25088,)))
-
+model.add(Dense(1024, activation='relu', input_shape=(input_shape,)))
 model.add(Dropout(0.5))
 model.add(Dense(512, activation='relu'))
 model.add(Dropout(0.5))
@@ -65,17 +55,16 @@ model.add(Dense(256, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
-model.add(Dense(101, activation='softmax'))
+model.add(Dense(2, activation='softmax'))
 
 #%%
 # defining a function to save the weights of best model
 
-from keras.callbacks import ModelCheckpoint
-
-mcp_save = ModelCheckpoint('weight.hdf5', save_best_only=True, monitor='val_loss', mode='min')
-# compiling the model
+from tensorflow.keras.callbacks import ModelCheckpoint
+filepath=".weights.h5"
+checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_weights_only=True,save_best_only=True, mode='min')
+callbacks_list = [checkpoint]
 
 model.compile(loss='categorical_crossentropy',optimizer='Adam',metrics=['accuracy'])
-
 # training the model
-model.fit(X_train, y_train, epochs=1, validation_data=(X_test, y_test), callbacks=[mcp_save], batch_size=128)
+model.fit(X_train, y_train, epochs=1, validation_data=(X_test, y_test), batch_size=16, callbacks=callbacks_list)
