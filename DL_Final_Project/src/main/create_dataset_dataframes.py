9ca56f5764e7c06c976ec983c0bbec594c7b932e
nenamalikah <nmb72@georgetown.edu>
2024-11-29 00:05:06 -0500

    Update create_dataset_dataframes.py

--- DL_Final_Project/src/main/create_dataset_dataframes.py
+++ DL_Final_Project/src/main/create_dataset_dataframes.py
@@ -5,16 +5,18 @@ sys.path.append('../')
 from components.utils import capture_frames
 
 #%%
-real = capture_frames(filepath='../../data/Celeb-real-sample',
-                      output_dir='../../data/Celeb-real-sample/Frames',
+real = capture_frames(filepath='../../data/Celeb-real',
+                      output_dir='../../data/Celeb-real/Frames',
                                   num_frames=5,
                                   label=1)
-print(f'Here is a sample of the Celeb-real-sample data: {real.head()}')
-real.to_excel('../../data/Celeb-real-sample.xlsx')
+print(f'Here is a sample of the Celeb-real data: {real.head()}')
+real.to_excel('../../data/Celeb-real.xlsx')
 
-fake = capture_frames(filepath='../../data/Celeb-synthesis-sample',
-                      output_dir='../../data/Celeb-synthesis-sample/Frames',
+fake = capture_frames(filepath='../../data/Celeb-synthesis',
+                      output_dir='../../data/Celeb-synthesis/Frames',
                                   num_frames=5,
                                   label=0)
-print(f'Here is a sample of the Celeb-synthesis-sample data: {fake.head()}')
-fake.to_excel('../../data/Celeb-synthesis-sample.xlsx')
+print(f'Here is a sample of the Celeb-synthesis data: {fake.head()}')
+fake.to_excel('../../data/Celeb-synthesis.xlsx')
+
+
