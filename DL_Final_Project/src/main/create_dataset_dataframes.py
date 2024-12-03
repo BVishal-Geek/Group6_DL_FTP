b442caebb59b79a14b89f1f2cc570bb8f3f42765
nenamalikah <nmb72@georgetown.edu>
2024-12-02 19:12:05 -0500

    Update create_dataset_dataframes.py

--- DL_Final_Project/src/main/create_dataset_dataframes.py
+++ DL_Final_Project/src/main/create_dataset_dataframes.py
@@ -5,17 +5,19 @@ sys.path.append('../')
 from components.utils import capture_frames
 
 #%%
+# 591 Videos
 real = capture_frames(filepath='../../data/Celeb-real',
                       output_dir='../../data/Celeb-real/Frames',
                                   num_frames=5,
-                                  label=1)
+                                  label=1) #THERE ARE 4713 IMAGES
 print(f'Here is a sample of the Celeb-real data: {real.head()}')
 real.to_excel('../../data/Celeb-real.xlsx')
 
+# 2341 Videos
 fake = capture_frames(filepath='../../data/Celeb-synthesis',
                       output_dir='../../data/Celeb-synthesis/Frames',
                                   num_frames=5,
-                                  label=0)
+                                  label=0) #THERE ARE 11655 IMAGES
 print(f'Here is a sample of the Celeb-synthesis data: {fake.head()}')
 fake.to_excel('../../data/Celeb-synthesis.xlsx')
 
