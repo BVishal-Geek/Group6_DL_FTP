535239a2a56d6f203362029b080c6b9e5d6985cd
nenamalikah <nmb72@georgetown.edu>
2024-12-07 11:11:09 -0500

    Update VGG16 train and test files

--- DL_Final_Project/src/main/VGG16/VGG16_train.py
+++ DL_Final_Project/src/main/VGG16/VGG16_train.py
@@ -1,30 +1,39 @@
 #%%
+import os
+
 import numpy as np
-from tensorflow.keras.callbacks import ModelCheckpoint
 from sklearn.model_selection import train_test_split
 
 from keras.utils import to_categorical
-from tensorflow.keras.applications import ResNet50
-from tensorflow.keras import optimizers
-
-from tensorflow.keras.models import Model
-
-from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import sys
 sys.path.append("../../")
 
 from components.utils import *
 #%%
-# ----- LOAD TRAIN IMAGES AND RESHAPE FOR MODELING -----
-print('----------LOADING TRAIN IMAGES----------')
+print('----------INSTANTIATING IMAGE GENERATORS----------')
 
-X = np.load('train_images.npy')
-y = np.load('y_train.npy')
-
-print('----------TRAIN IMAGES LOADED----------')
-y = to_categorical(y, num_classes=2)
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=6303, test_size=0.2)
-
+train_directory = '../../../data/Frames_train'
+valid_directory = '../../../data/Frames_valid'
+BATCH_SIZE = 32
+generator = ImageDataGenerator()
+train_generator = generator.flow_from_directory(
+    directory=train_directory,
+    target_size=(224, 224),
+    batch_size=BATCH_SIZE,
+    class_mode="binary",
+    shuffle=True,
+    seed=6303
+)
+print(f'\n\n length of train gen: {train_generator.n}\n\n')
+valid_generator = generator.flow_from_directory(
+    directory=valid_directory,
+    target_size=(224, 224),
+    batch_size=BATCH_SIZE,
+    class_mode="binary",
+    shuffle=True,
+    seed=6303)
 #%%
 base_model = FineTuneModel(model_name='VGG16', input_shape=(224, 224, 3), num_classes=2)
 
@@ -33,13 +42,23 @@ base_model.add_custom_layers()
 base_model.freeze_base_layers()
 base_model.compile_model(learning_rate=0.0001)
 
-base_history = base_model.train_model((X_train, y_train), (X_test, y_test), epochs=15, batch_size=16)
+steps_per_epoch = train_generator.n // BATCH_SIZE
+validation_steps = valid_generator.n // BATCH_SIZE
+print(f'----------TRAIN STEPS PER EPOCH: {steps_per_epoch}----------')
+print(f'----------VALIDATION STEP: {validation_steps}----------')
+
+base_history = base_model.train_model(train_generator, valid_generator, epochs=15, steps_per_epoch=steps_per_epoch, validation_steps=validation_steps)
 plot_loss(history=base_history, model_name='VGG16', layers_info='Pretrained Layers Frozen', image_name='VGG16_pretrained')
 
 #%%
 base_model.unfreeze_layers(num_layers=30)
 base_model.compile_model(optimizer='adamw',learning_rate=0.00001)
-base_history_ft = base_model.train_model((X_train, y_train), (X_test, y_test), epochs=25, batch_size=16)
+
+early_stopping = EarlyStopping(monitor='val_loss', patience=14, restore_best_weights=True)
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.000005,
+                              patience=7, min_lr=0.00001)
+
+base_history_ft = base_model.train_model(train_generator, valid_generator, epochs=25, steps_per_epoch=steps_per_epoch, validation_steps=validation_steps, callbacks=[early_stopping, reduce_lr])
 plot_loss(history=base_history_ft, model_name='VGG16', layers_info='Last 30 Layers Unfrozen', image_name='VGG16_finetuned')
 
 # Save the fine-tuned model
